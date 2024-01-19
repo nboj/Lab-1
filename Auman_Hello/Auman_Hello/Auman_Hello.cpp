11734a704e041d4373722d99dbcedde9f0c4f441
Christian <christian.auman@outlook.com>
2024-01-19 12:36:30 -0500

    initial commit

---
+++ Auman_Hello/Auman_Hello/Auman_Hello.cpp
@@ -0,0 +1,14 @@
+// required in order to use std::cout
+#include <iostream> 
+using namespace std;
+
+/*
+This is a simple hello world program that prints out the a string to the console.
+@author Christian Auman
+@version 1.0.1
+@since 2024-1-19
+*/
+int main() { 
+	// prints the string to console with cout
+	cout << "Hello my name is Christian Auman!"; 
+}
---
+++ ex1.4/ex1-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+using namespace std;
+
+/*
+This program prints a string to the console and displays a face.
+@author Christian
+@version 1.0.0
+@since 2024-1-19
+*/
+int main() {
+	// prints to the console
+	cout << "North Canton, Friday .....\nThis is my first c++ program.\n\n^   ^\n  o\n --- \n";
+}
